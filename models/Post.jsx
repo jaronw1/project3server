c3c1f8bf60edb2c0a880a2036badfb0c733b25b0
jaronw1 <jaronwong6@gmail.com>
2023-05-03 10:38:19 -0700

    erd stuff

--- models/Post.jsx
+++ models/Post.jsx
@@ -7,9 +7,26 @@ const PostSchema = new mongoose.Schema({
     userId: {
         Number
     },
+    isReview: {
+        Boolean
+    },
     postTitle: {
         String
+    },
+    postBody: {
+        String
+    },
+    taggedGame: {
+        Number
+    },
+    rating: {
+        Number
+    },
+    imageUrl: {
+        String
     }
-    
 
+}, {
+	timestamps: true
 })
+module.exports = mongoose.model('Post', PostSchema)
